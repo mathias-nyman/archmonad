4f2094158f765687f1efc7a1080dee67905b452f
Mathias Nyman <None>
2014-01-05 18:46:49 +0200

    Add dotfiles to users home directories.

--- releng/root-image/root/customize_root_image.sh
+++ releng/root-image/root/customize_root_image.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e -u
+USER=arch
 
 sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
@@ -10,7 +11,7 @@ ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 usermod -s /usr/bin/zsh root
 cp -aT /etc/skel/ /root/
 
-useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /usr/bin/zsh arch
+useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /usr/bin/zsh $USER
 
 chmod 750 /etc/sudoers.d
 chmod 440 /etc/sudoers.d/g_wheel
@@ -19,3 +20,10 @@ sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 
 systemctl enable multi-user.target pacman-init.service choose-mirror.service
+
+# Copy files over to home
+for user in root $USER
+do
+    su -c "git clone --recursive https://github.com/mathias-nyman/dotfiles.git ~$user/dotfiles" $user
+    su -c "cd ~$user/dotfiles && ./install.sh force" $user
+done
